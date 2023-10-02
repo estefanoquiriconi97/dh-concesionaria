3fd01c9c8c05fa01e0f59721d8356d95f2bab49f
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2023-10-02 15:11:07 -0300

    puede comprar?

--- app.js
+++ app.js
@@ -19,5 +19,13 @@ const concesionaria = {
     return this.autos.filter(auto => auto.vendido).map(auto =>{
         return auto.precio;
     })
+  },
+  totalDeVentas(){
+    return this.listaDeVentas().reduce((acumulador, venta) => acumulador + venta, 0);
+  },
+  puedeComprar(auto, persona){
+    return auto.precio <= persona.capacidadDePagoTotal && (auto.precio / auto.cuotas) <= persona.capacidadDePagoEnCuotas;
   }
 };
+
+console.log(concesionaria.totalDeVentas());
