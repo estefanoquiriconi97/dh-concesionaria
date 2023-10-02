6ac0970b797824a05f7ddf6affb31f1819f8e807
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2023-10-02 14:26:18 -0300

    Etapa 6 completada: autos para la venta

--- app.js
+++ app.js
@@ -8,5 +8,11 @@ const concesionaria = {
   venderAuto(patente){
     const auto = this.buscarAuto(patente);
     return auto ? (auto.vendido = true, auto) : null;
+  },
+  autosParaLaVenta(){
+    return this.autos.filter(auto => auto.vendido === false);
   }
 };
+
+
+console.log(concesionaria.autosParaLaVenta());
