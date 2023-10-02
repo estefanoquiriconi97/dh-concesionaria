8cbfccbc02fb01235eb17ad41f1e87b167dcc047
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2023-10-02 14:18:20 -0300

    optimizando código

--- app.js
+++ app.js
@@ -2,26 +2,11 @@ const autosImportados = require("./autos.js");
 
 const concesionaria = {
   autos: autosImportados,
-  buscarAuto: function (patente) {
-    let encontrado = this.autos.find((auto) => auto.patente === patente);
-    if (encontrado) {
-      return encontrado;
-    } else {
-      return null;
-    }
+  buscarAuto(patente){
+    return this.autos.find(auto => auto.patente === patente) || null;
   },
-  venderAuto: function (patente) {
-    let auto = this.buscarAuto(patente);
-    if(auto){
-        auto.vendido = true;
-        return auto;
-    }else{
-        return null;
-    }
-  },
+  venderAuto(patente){
+    const auto = this.buscarAuto(patente);
+    return auto ? (auto.vendido = true, auto) : null;
+  }
 };
-
-console.log(concesionaria.autos);
-
-console.log(concesionaria.venderAuto("JJK116"));
-console.log(concesionaria.autos);
